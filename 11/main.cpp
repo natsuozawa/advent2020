44460e031b41eaadfc587225b833ac0c0c8784e6
natsuozawa <natsuozawa@outlook.com>
2020-12-06 14:29:49 +0900

    Fix naming mistake

--- 11/main.cpp
+++ 11/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <set>
 #include <string>
-#include <unordered_map>
 
 using namespace std;
 
@@ -8,21 +8,16 @@ int main() {
   int ans = 0;
 
   while (!cin.eof()) {
-    unordered_map<char, int> s;
-    int cnt = 0;
+    set<char> s;
     while (true) {
       string t;
       getline(cin, t);
       if (t == "") break;
       for (int i = 0; i < t.size(); i++) {
-        s[t.at(i)]++;
+        s.insert(t.at(i));
       }
-      cnt++;
     }
-    
-    for (char i = 'a'; i <= 'z'; i++) {
-      if (s[i] == cnt) ans++;
-    }
+    ans += s.size();
   }
 
   cout << ans << endl;
--- 12/main.cpp
+++ 12/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <set>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,16 +8,21 @@ int main() {
   int ans = 0;
 
   while (!cin.eof()) {
-    set<char> s;
+    unordered_map<char, int> s;
+    int cnt = 0;
     while (true) {
       string t;
       getline(cin, t);
       if (t == "") break;
       for (int i = 0; i < t.size(); i++) {
-        s.insert(t.at(i));
+        s[t.at(i)]++;
       }
+      cnt++;
     }
-    ans += s.size();
+    
+    for (char i = 'a'; i <= 'z'; i++) {
+      if (s[i] == cnt) ans++;
+    }
   }
 
   cout << ans << endl;
